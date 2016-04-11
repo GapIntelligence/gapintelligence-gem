115dffa1996eddb8328bba03ca0cfe981ff05447
Timur Gabdrakipov <gtimti@gmail.com>
2016-04-11 22:35:40 +0500

    Add creating download
    https://www.pivotaltracker.com/story/show/115345279

--- lib/gap_intelligence/client/downloads.rb
+++ lib/gap_intelligence/client/downloads.rb
@@ -7,5 +7,13 @@ module GapIntelligence
     def downloads(options = {}, &block)
       perform_request(:get, 'downloads', options.merge(record_class: Download), &block)
     end
+
+    # Creates new download
+    #
+    # @param options [Hash] the options to make the request with
+    # @yield [req] The Faraday request
+    def create_download(options, &block)
+      perform_request(:post, 'downloads', options.merge(record_class: Download), &block)
+    end
   end
 end
