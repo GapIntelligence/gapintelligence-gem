a444b550456f4e079ebc2dbe6e40f3ba351556a4
Timur Gabdrakipov <gtimti@gmail.com>
2016-04-12 21:30:42 +0500

    Require errors before other classes

--- lib/gap_intelligence.rb
+++ lib/gap_intelligence.rb
@@ -2,12 +2,13 @@ require 'gap_intelligence/version'
 
 require 'oauth2'
 
+# Errors
+require 'gap_intelligence/errors/configuration_error'
+require 'gap_intelligence/errors/authentication_error'
+require 'gap_intelligence/errors/request_error'
+
 require 'gap_intelligence/configuration'
 require 'gap_intelligence/models/record'
 require 'gap_intelligence/models/download'
 require 'gap_intelligence/models/record_set'
 require 'gap_intelligence/client'
-
-# Errors
-require 'gap_intelligence/errors/configuration_error'
-require 'gap_intelligence/errors/authentication_error'
