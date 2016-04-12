b67bc550179c670bffab10ca7ac0c524472db4b2
Timur Gabdrakipov <gtimti@gmail.com>
2016-04-12 16:00:05 +0500

    Add body param

--- lib/gap_intelligence/client/downloads.rb
+++ lib/gap_intelligence/client/downloads.rb
@@ -3,17 +3,18 @@ module GapIntelligence
     # Requests and returns downloads
     #
     # @param options [Hash] the options to make the request with
-    # @yield [req] The Faraday request
+    # @yield [request] The Faraday request
     def downloads(options = {}, &block)
       perform_request(:get, 'downloads', options.merge(record_class: Download), &block)
     end
 
     # Creates new download
     #
+    # @param body [Hash, String] the body of the request
     # @param options [Hash] the options to make the request with
-    # @yield [req] The Faraday request
-    def create_download(options, &block)
-      perform_request(:post, 'downloads', options.merge(record_class: Download), &block)
+    # @yield [request] The Faraday request
+    def create_download(body, options = {}, &block)
+      perform_request(:post, 'downloads', options.merge(body: body, record_class: Download), &block)
     end
   end
 end
