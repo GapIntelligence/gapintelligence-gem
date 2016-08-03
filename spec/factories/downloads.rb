1ab6b60e73dfb17dd35a734e404ff14c83ee61bd
Timur Gabdrakipov <gtimti@gmail.com>
2016-08-03 19:40:31 +0500

    Update download factory

--- spec/factories/downloads.rb
+++ spec/factories/downloads.rb
@@ -1,16 +1,19 @@
 FactoryGirl.define do
   factory :download, class: Hash do
+    owner 'Outsider'
+    owner_id 1
     start_at '2015-11-24'
     end_at '2016-02-23'
     created_at Time.now.to_s
+    categories ['1']
     category_names 'TVs'
+    countries ['US']
     country_names ['United States']
-    report_types ['Pricings']
-    merchant_names ['Walgreens']
-    brand_names ['HP']
+    report_types ['pricings']
+    report_type_names ['Pricings']
+    merchants ['Walgreens']
+    brands ['HP']
     channels []
-    pricing_date 'date_collected'
-    pricing_date_type 'raw'
     advertisements_headers ['Category', 'Part Number', 'Product']
     pricings_headers ['Category', 'Part Number', 'Product']
     promotions_headers ['Category', 'Part Number', 'Product']
@@ -18,12 +21,12 @@ FactoryGirl.define do
     included_spec_headers ['Smart TV', 'HDR']
     standalone_core_spec_headers ['WiFi', '3D']
     standalone_spec_headers ['Launch Date', 'Width', 'Height']
+    product_info_fields_headers []
+    pricing_date 'date_collected'
+    pricing_date_type 'raw'
     file_type 'csv'
     custom_file_name ''
     status 'done'
-
-    attachment 'https://gapi.s3.amazonaws.com/uploads/download/attachment/2480/tvs-pricings-20151124-20160223.zip'
-    product_info_fields_headers []
 
     initialize_with {
       {
