8e16645e11c42fc024100bc88b9805d9e5ff7ba8
Timur Gabdrakipov <gtimti@gmail.com>
2017-05-24 11:41:28 +0500

    Remove extra params argument

--- lib/gap_intelligence/client/merchant_pricing_trend_downloads.rb
+++ lib/gap_intelligence/client/merchant_pricing_trend_downloads.rb
@@ -32,12 +32,11 @@ module GapIntelligence
     #
     # @param owner_id [String,Integer] owner id of the merchant pricing trend downloads
     # @param download_id [String,Integer] id of the merchant pricing trend download
-    # @param params [Hash] parameters of the http request
     # @param options [Hash] options for the http request
     # @yield [request] The Faraday request
     # @return [MerchantPricingTrendDownload] the requested merchant pricing trend download
     # @see https://api.gapintelligence.com/api/doc/v1/merchant_pricing_trend_downloads/show.html
-    def merchant_pricing_trend_download(owner_id, download_id, params = {}, options = {}, &block)
+    def merchant_pricing_trend_download(owner_id, download_id, options = {}, &block)
       default_option(options, :record_class, MerchantPricingTrendDownload)
       perform_request(:get, build_resource_path('merchant_pricing_trend_downloads', download_id), options.merge(params: params.merge(owner_id: owner_id)), &block)
     end
