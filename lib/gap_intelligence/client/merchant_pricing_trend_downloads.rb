38c5f92ccfeea2e6e37594c20c5da80c65a37f5d
Timur Gabdrakipov <gtimti@gmail.com>
2017-05-24 10:45:38 +0500

    Add MPTD show

--- lib/gap_intelligence/client/merchant_pricing_trend_downloads.rb
+++ lib/gap_intelligence/client/merchant_pricing_trend_downloads.rb
@@ -28,6 +28,20 @@ module GapIntelligence
       perform_request(:get, 'merchant_pricing_trend_downloads', options.merge(params: params.merge(owner_id: owner_id)), &block)
     end
 
+    # Requests and returns a merchant pricing trend download
+    #
+    # @param owner_id [String,Integer] owner id of the merchant pricing trend downloads
+    # @param download_id [String,Integer] id of the merchant pricing trend download
+    # @param params [Hash] parameters of the http request
+    # @param options [Hash] options for the http request
+    # @yield [request] The Faraday request
+    # @return [MerchantPricingTrendDownload] the requested merchant pricing trend download
+    # @see https://api.gapintelligence.com/api/doc/v1/merchant_pricing_trend_downloads/show.html
+    def merchant_pricing_trend_download(owner_id, download_id, params = {}, options = {}, &block)
+      default_option(options, :record_class, MerchantPricingTrendDownload)
+      perform_request(:get, build_resource_path('merchant_pricing_trend_downloads', download_id), options.merge(params: params.merge(owner_id: owner_id)), &block)
+    end
+
     # Deletes merchant pricing trend downloads
     #
     # @param owner_id [String,Integer] owner id of the merchant pricing trend downloads
