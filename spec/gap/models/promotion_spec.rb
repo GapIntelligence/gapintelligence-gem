4ab645bb6d345613b0bb84b9685e6155292eaf6f
timur <iamsuleymanov@gmail.com>
2017-07-21 17:57:01 +0500

    refactoring

--- spec/gap/models/promotion_spec.rb
+++ spec/gap/models/promotion_spec.rb
@@ -5,27 +5,27 @@ describe GapIntelligence::Promotion do
 
   let!(:attributes_list) {
     [
-              :start_date,
-         :end_date,
-         :promotion_type,
-         :value,
-         :bundle_type,
-         :merchant_sku,
-         :notes
-         :category_name
- :brand
- :part_number
- :product_name
-  :merchant
-  :deleted
+      :start_date,
+      :end_date,
+      :promotion_type,
+      :value,
+      :bundle_type,
+      :merchant_sku,
+      :notes
+      :category_name
+      :brand
+      :part_number
+      :product_name
+      :merchant
+      :deleted
     ]
   }
 
   describe 'attributes' do
     subject(:promotion) { described_class.new build(:promotion) }
 
-      it 'has correct attributes' do
-        expect(category).to include(attributes_list)
-      end
+    it 'has correct attributes' do
+      expect(category).to include(attributes_list)
+    end
   end
 end
