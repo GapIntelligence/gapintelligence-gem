77d08e7fb9b9ef6bcb434acaa57ead17bb3c15d0
iamsuleyman <iamsuleymanov@gmail.com>
2017-12-06 13:49:14 +0500

    ~

--- lib/gap_intelligence/models/category.rb
+++ lib/gap_intelligence/models/category.rb
@@ -4,6 +4,6 @@ module GapIntelligence
     attribute :full_name
     attribute :frequency
     attribute :published_date, class: Date
-    attribute :publish_tad_only
+    attribute :publish_tag_only
   end
 end
