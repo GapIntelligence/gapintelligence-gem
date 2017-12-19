c6b3814ea5c0aa0d4ed4b9ea83557bbc6ac6025a
Timur Suleymanov <iamsuleymanov@gmail.com>
2017-12-19 16:51:01 +0500

    remove default attribute from brands (#52)
    https://www.pivotaltracker.com/story/show/153600887

--- lib/gap_intelligence/client/brands.rb
+++ lib/gap_intelligence/client/brands.rb
@@ -1,14 +1,14 @@
 module GapIntelligence
   # @see https://api.gapintelligence.com/api/doc/v1/brands.html
   module Brands
-    # Requests a list of merchants
+    # Requests a list of brands
     #
     # @param params [Hash] parameters of the http request
     # @param options [Hash] the options to make the request with
     # @yield [req] The Faraday request
-    # @return [RecordSet<Brand>] the requested merchants
+    # @return [RecordSet<Brand>] the requested brands
     # @see https://api.gapintelligence.com/api/doc/v1/brands/index.html
-    def brands(params = {'category_name' => ''}, options = {}, &block)
+    def brands(params = {}, options = {}, &block)
       default_option(options, :record_class, Brand)
       perform_request(:get, 'brands', options.merge(params: params), &block)
     end
