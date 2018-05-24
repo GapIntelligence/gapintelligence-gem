fafdff4dbc0372e690816a3c724f674e05143c7d
Aileen Cacayorin <aileen.cacayorin@gmail.com>
2018-05-24 14:03:57 -0700

    add product location attribute to category (#55)
    * add product location attribute to category
    
    * add product location to category spec

--- spec/gap/models/category_spec.rb
+++ spec/gap/models/category_spec.rb
@@ -25,5 +25,8 @@ describe GapIntelligence::Category do
     it 'has publish tag only' do
       expect(category).to respond_to(:publish_tag_only)
     end
+    it 'has publish product location only' do
+      expect(category).to respond_to(:publish_product_location)
+    end
   end
 end
