a4f88138bdef3e39b58108ac2658942421ca9e66
Timur Gabdrakipov <gtimti@gmail.com>
2019-08-21 13:27:17 -0700

    Add ad page endpoint

--- lib/gap_intelligence/client/ad_pages.rb
+++ lib/gap_intelligence/client/ad_pages.rb
@@ -1,6 +1,18 @@
 module GapIntelligence
   # @see https://api.gapintelligence.com/api/doc/v1/ad_pages.html
   module AdPages
+    # Requests and returns an ad pages
+    #
+    # @param ad_image_id [String] ad image id
+    # @param options [Hash] the options to make the request with
+    # @yield [req] The Faraday request
+    # @return [RecordSet<AdPage>] collection of ad pages
+    # @see https://api.gapintelligence.com/api/doc/v1/ad_pages/index.html
+    def ad_pages(ad_image_id, options = {}, &block)
+      default_option(options, :record_class, AdPage)
+      perform_request(:get, build_resource_path('ad_images', ad_image_id, 'ad_pages'), options, &block)
+    end
+
     # Requests and returns an ad page
     #
     # @param ad_page_id [String] ad page public id
