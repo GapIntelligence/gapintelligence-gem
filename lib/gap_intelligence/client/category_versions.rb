ddf71e212389792925518265cf04c7a0f995eb66
Timur Gabdrakipov <gtimti@gmail.com>
2020-05-29 10:45:37 -0700

    Update record class

--- lib/gap_intelligence/client/category_versions.rb
+++ lib/gap_intelligence/client/category_versions.rb
@@ -7,7 +7,7 @@ module GapIntelligence
     # @return [RecordSet<CategoryVersion>] the requested country
     # @see https://api.gapintelligence.com/api/doc/v1/category_versions/index.html
     def category_versions(params = {}, options = {}, &block)
-      default_option(options, :record_class, Country)
+      default_option(options, :record_class, CategoryVersion)
       perform_request(:get, 'category_versions', options.merge(params: params), &block)
     end
   end
