94e2c7f75aca7cc68301f0f2f484dccc0e64afb8
iamsuleyman <iamsuleymanov@gmail.com>
2020-06-03 16:08:09 +0500

    add category_id attr

--- lib/gap_intelligence/models/category_version.rb
+++ lib/gap_intelligence/models/category_version.rb
@@ -2,6 +2,7 @@ module GapIntelligence
   class CategoryVersion < Record
     attribute :name
     attribute :full_name
+    attribute :category_id
     attribute :frequency
     attribute :published_date, class: Date
     attribute :publish_tag_only
