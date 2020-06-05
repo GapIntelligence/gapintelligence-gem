4a8b588bcf93bc10d66cab56590acb692fe846c4
iamsuleyman <iamsuleymanov@gmail.com>
2020-06-05 17:51:01 +0500

    update specs

---
+++ spec/factories/category_versions.rb
@@ -0,0 +1,17 @@
+FactoryGirl.define do
+  factory :category_version, class: Hash do
+    initialize_with {
+      {
+        "id" => 1,
+        "type" => "categories",
+        "attributes" => { 'name' => 'A Category',
+                          'full_name' => 'A Category Full Name',
+                          'category_id' => 1,
+                          'frequency' => 'weekly',
+                          'frequency' => 'weekly',
+                          'published_date' => '2000-01-01',
+                          'country_code' => 'US' }
+      }
+    }
+  end
+end
