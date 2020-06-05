c09161efd88ddea5dfbf62a0dc17fc617997c3ab
iamsuleyman <iamsuleymanov@gmail.com>
2020-06-05 17:43:01 +0500

    fix specs

--- spec/gap/models/category_version_spec.rb
+++ spec/gap/models/category_version_spec.rb
@@ -7,27 +7,27 @@ describe GapIntelligence::CategoryVersion do
     subject(:category_version) { described_class.new build(:category_version) }
 
     it 'has name' do
-      expect(category).to respond_to(:name)
+      expect(category_version).to respond_to(:name)
     end
 
     it 'has full_name' do
-      expect(category).to respond_to(:full_name)
+      expect(category_version).to respond_to(:full_name)
     end
 
     it 'has frequency' do
-      expect(category).to respond_to(:frequency)
+      expect(category_version).to respond_to(:frequency)
     end
 
     it "has published date" do
-      expect(category.published_date).to be_an_instance_of(Date)
+      expect(category_version.published_date).to be_an_instance_of(Date)
     end
 
     it 'has publish product location only' do
-      expect(category).to respond_to(:publish_product_location)
+      expect(category_version).to respond_to(:publish_product_location)
     end
 
     it 'has publish product location only' do
-      expect(category).to respond_to(:country_code)
+      expect(category_version).to respond_to(:country_code)
     end
   end
 end
