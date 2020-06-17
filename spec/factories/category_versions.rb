466af2f07780995f57fb29480f7153f7de288eca
Timur Suleymanov <iamsuleymanov@gmail.com>
2020-06-17 23:13:53 +0500

    Add category versions endpoint (#64)
    https://www.pivotaltracker.com/story/show/171750067

---
+++ spec/factories/category_versions.rb
@@ -0,0 +1,18 @@
+FactoryGirl.define do
+  factory :category_version, class: Hash do
+    initialize_with {
+      {
+        "id" => 1,
+        "type" => "categories",
+        "attributes" => { 'name' => 'A Category',
+                          'full_name' => 'A Category Full Name',
+                          'display_name' => 'A display name',
+                          'category_id' => 1,
+                          'frequency' => 'weekly',
+                          'frequency' => 'weekly',
+                          'published_date' => '2000-01-01',
+                          'country_code' => 'US' }
+      }
+    }
+  end
+end
---
+++ spec/gap/models/category_version_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe GapIntelligence::CategoryVersion do
+  include_examples 'Record'
+
+  describe 'attributes' do
+    subject(:category_version) { described_class.new build(:category_version) }
+
+    it 'has name' do
+      expect(category_version).to respond_to(:name)
+    end
+
+    it 'has full_name' do
+      expect(category_version).to respond_to(:full_name)
+    end
+
+    it 'has display_name' do
+      expect(category_version).to respond_to(:display_name)
+    end
+
+    it 'has frequency' do
+      expect(category_version).to respond_to(:frequency)
+    end
+
+    it "has published date" do
+      expect(category_version.published_date).to be_an_instance_of(Date)
+    end
+
+    it 'has publish product location only' do
+      expect(category_version).to respond_to(:publish_product_location)
+    end
+
+    it 'has country code' do
+      expect(category_version).to respond_to(:country_code)
+    end
+
+    it 'has report types' do
+      expect(category_version).to respond_to(:report_types)
+    end
+  end
+end
