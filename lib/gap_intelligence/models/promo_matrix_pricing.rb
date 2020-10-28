8168178548e42d19ef9da25a655320c7063b5dc8
Timur Gabdrakipov <gtimti@gmail.com>
2020-10-28 15:19:47 -0700

    Update models

--- lib/gap_intelligence/models/promo_matrix_pricing.rb
+++ lib/gap_intelligence/models/promo_matrix_pricing.rb
@@ -1,5 +1,9 @@
 module GapIntelligence
   class PromoMatrixPricing < Record
-    attribute :name
+    attributes :product_version_id, :merchant_id
+    attribute :date, class: Date
+    attributes :shelf_price, :net_price
+    attributes :promo_value, :promo_percentage
+    attributes :on_ad, :on_promo
   end
 end
