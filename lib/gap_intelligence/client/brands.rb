645d5eeb0dc91704ef733c02e88e3fff8e4c68ce
AbduQahhor <abduqahhor.muhamadjonov@gmail.com>
2022-01-04 17:22:01 +0500

    add printer brands request

--- lib/gap_intelligence/client/brands.rb
+++ lib/gap_intelligence/client/brands.rb
@@ -12,5 +12,17 @@ module GapIntelligence
       default_option(options, :record_class, Brand)
       perform_request(:get, 'brands', options.merge(params: params), &block)
     end
+
+    # Requests a list of printer brands
+    #
+    # @param params [Hash] parameters of the http request
+    # @param options [Hash] the options to make the request with
+    # @yield [req] The Faraday request
+    # @return [RecordSet<Brand>] the requested brands
+    # @see https://api.gapintelligence.com/api/doc/v1/brands/printer_brands.html
+    def brands(params = {}, options = {}, &block)
+      default_option(options, :record_class, Brand)
+      perform_request(:get, 'brands/printer_brands', options.merge(params: params), &block)
+    end
   end
 end
