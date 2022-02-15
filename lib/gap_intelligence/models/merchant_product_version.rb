3934945649ae1ad273cbe3a22309a76af868638f
Timur Gabdrakipov <gtimti@gmail.com>
2022-02-14 16:22:00 -0800

    Add category version

--- lib/gap_intelligence/models/merchant_product_version.rb
+++ lib/gap_intelligence/models/merchant_product_version.rb
@@ -1,5 +1,6 @@
 module GapIntelligence
   class MerchantProductVersion < Record
+    attributes :category_version_id, :category_version_name
     attributes :merchant_id, :merchant_name
     attributes :brand_id, :brand_name
     attributes :product_version_id, :part_number, :product_name
