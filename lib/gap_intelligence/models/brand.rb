ff68065986a32005bdd5a66a098201a1557c36c6
Timur Gabdrakipov <gtimti@gmail.com>
2022-02-28 14:15:49 -0800

    Update brands

--- lib/gap_intelligence/models/brand.rb
+++ lib/gap_intelligence/models/brand.rb
@@ -1,10 +1,5 @@
 module GapIntelligence
   class Brand < Record
     attribute :name
-
-    def initialize(brand_name)
-      @raw = brand_name
-      @attributes = { 'name' => brand_name }
-    end
   end
 end
