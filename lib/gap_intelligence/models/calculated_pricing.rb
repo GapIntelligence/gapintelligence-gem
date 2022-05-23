992795b183f746ddae444973c0834547c39466d6
AbduQahhor <abduqahhor.muhamadjonov@gmail.com>
2022-05-23 17:40:17 +0500

    add stock & prev price fields to calc pricings

--- lib/gap_intelligence/models/calculated_pricing.rb
+++ lib/gap_intelligence/models/calculated_pricing.rb
@@ -5,5 +5,6 @@ module GapIntelligence
     attributes :shelf_price, :net_price
     attributes :promo_value, :promo_percentage
     attributes :on_ad, :on_promo
+    attributes :in_stock, :previous_shelf_price, :previous_net_price
   end
 end
