7f78d5d139f03d63c6adb3b8c3ff963b9503bced
Timur Gabdrakipov <gtimti@gmail.com>
2022-09-13 14:12:55 +0500

    Add delta agg record
    https://www.pivotaltracker.com/story/show/182903797

--- lib/gap_intelligence/client/delta.rb
+++ lib/gap_intelligence/client/delta.rb
@@ -6,11 +6,10 @@ module GapIntelligence
     # @param params [Hash] parameters of the http request
     # @param options [Hash] the options to make the request with
     # @yield [req] The Faraday request
-    # @return [Record] the list of requested data
+    # @return [DeltaAggregateRecord] the list of requested data
     # @see https://api.gapintelligence.com/api/doc/v1/delta/aggregate.html
     def delta_aggregate(params = {}, options = {}, &block)
-      options[:init_with_response_body] = true
-      default_option(options, :record_class, Record)
+      default_option(options, :record_class, DeltaAggregateRecord)
       perform_request(:post, 'delta/aggregate', options.merge(body: params), &block)
     end
   end
---
+++ lib/gap_intelligence/models/delta_aggregate_record.rb
@@ -0,0 +1,5 @@
+module GapIntelligence
+  class DeltaAggregateRecord < Record
+    attributes :x, :y, :values, class: Array
+  end
+end
