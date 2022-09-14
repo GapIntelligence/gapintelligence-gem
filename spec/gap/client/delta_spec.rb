55dbbbe459447bb30ac2737cfc2619ab68b00ed9
Aileen Bull <aileen.cacayorin@gmail.com>
2022-09-14 15:27:44 -0500

    Feature/delta 182903797 (#84)
    * Add delta agg record
    
    https://www.pivotaltracker.com/story/show/182903797
    
    * Add meta to record
    
    * Update attributes
    
    Co-authored-by: Timur Gabdrakipov <gtimti@gmail.com>

--- spec/gap/client/delta_spec.rb
+++ spec/gap/client/delta_spec.rb
@@ -5,18 +5,18 @@ describe GapIntelligence::Delta do
   subject(:client) { GapIntelligence::Client.new(client_id: 'CLIENTID', client_secret: 'ASECRET') }
 
   describe '#delta_aggregate' do
-    let(:params){ { category_versions: [1], start_at: '2020-01-01', end_at: '2020-02-01', type: 'pricings', x: 'brand_id', y: 'merchant_id' } }
-    before { stub_api_request(:post, url: '/delta/aggregate', response: { data: build(:delta_aggregation, x: [], y: []) }) }
+    let(:params){ { category_versions: [1], start_at: '2020-01-01', end_at: '2020-02-01', type: 'pricings', x: 'brand', y: 'merchant' } }
+    before { stub_api_request(:post, url: '/delta/aggregate', response: { data: build(:delta_aggregation, x: [{ id: 1, name: 'Brand 1' }], y: [{ id: 1, name: 'Merchant' }], values: [{ x: 1, y: 1, values: 1 }]) }) }
     subject(:record) { client.delta_aggregate(params) }
 
     it 'requests the endpoint' do
       client.delta_aggregate(params)
 
-      expect(api_post('/delta/aggregate').with(body: { category_versions: [1.to_s], start_at: '2020-01-01', end_at: '2020-02-01', type: 'pricings', x: 'brand_id', y: 'merchant_id' })).to have_been_made
+      expect(api_post('/delta/aggregate').with(body: { category_versions: [1.to_s], start_at: '2020-01-01', end_at: '2020-02-01', type: 'pricings', x: 'brand', y: 'merchant' })).to have_been_made
     end
 
     it 'returns record' do
-      expect(record).to be_instance_of(GapIntelligence::Record)
+      expect(record).to be_instance_of(GapIntelligence::DeltaAggregateRecord)
     end
   end
 end
