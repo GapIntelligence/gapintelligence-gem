4ca76fd0ad14555514c0516a49c3af936452bfae
Timur Gabdrakipov <gtimti@gmail.com>
2022-11-16 09:57:28 -0800

    Update client

--- lib/gap_intelligence/client/delta.rb
+++ lib/gap_intelligence/client/delta.rb
@@ -12,5 +12,17 @@ module GapIntelligence
       default_option(options, :record_class, DeltaAggregateRecord)
       perform_request(:post, 'delta/aggregate', options.merge(body: params), &block)
     end
+
+    # Requests delta pricings data
+    #
+    # @param params [Hash] parameters of the http request
+    # @param options [Hash] the options to make the request with
+    # @yield [req] The Faraday request
+    # @return [DeltaPricing] the list of requested data
+    # @see https://api.gapintelligence.com/api/doc/v1/delta/pricings.html
+    def delta_pricings(params = {}, options = {}, &block)
+      default_option(options, :record_class, DeltaPricing)
+      perform_request(:post, 'delta/pricings', options.merge(body: params), &block)
+    end
   end
 end
