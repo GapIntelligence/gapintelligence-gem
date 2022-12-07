a4819353456a279b6a825e3a25ca2eb81f823588
Timur Gabdrakipov <gtimti@gmail.com>
2022-12-07 12:50:40 -0800

    Add Delta Ad, Pricing and Promo endpoints (#85)
    https://www.pivotaltracker.com/story/show/183364570

---
+++ lib/gap_intelligence/models/delta_pricing.rb
@@ -0,0 +1,17 @@
+module GapIntelligence
+  class DeltaPricing < Record
+    attributes :merchant_id, :category_id, :category_version_id,
+               :brand_id, :product_id, :product_version_id
+
+    attributes :category_name, :country_code, :category_version_name
+    attributes :merchant_name, :merchant_channel
+    attributes :brand_name, :part_number, :product_version_name
+    attributes :product_status, :product_location, :specifications
+
+    attributes :date, class: Date
+    attributes :shelf_price, :net_price
+    attributes :promo_value, :promo_percentage
+    attributes :on_ad, :on_promo, :in_stock
+    attributes :product_location, :merchant_debut_date, :age_of_placements_wks
+  end
+end
